e793365f9209a14d052474f10e650d11b9fac4fa
YongKi Kim <cloudeyes@gmail.com>
2015-09-22 22:42:16 +0900

    Added chapter17

---
+++ chapter17/src/test/scala/cloudeyes/study/scala/chapter17/TestChapter17.scala
@@ -0,0 +1,15 @@
+package cloudeyes.study.scala.chapter17
+
+import org.scalatest.FunSuite
+import org.scalatest.junit._
+import org.scalatest.Assertions._
+import org.junit.runner.RunWith
+import org.junit.Assert._
+
+@RunWith(classOf[JUnitRunner])
+class TestChapter17 extends FunSuite {
+
+  test ("a simple test") {
+    assertEquals("should", "fail")
+  }
+}
